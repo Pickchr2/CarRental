8a5a4e73b1c219f922d25559a44bfd459a99a052
Chris Pickens <Cpickens1270@gmail.com>
2023-04-03 01:53:14 -0600

    Code Clear button

--- CarRental/CarRentalForm.vb
+++ CarRental/CarRentalForm.vb
@@ -12,7 +12,24 @@ Class CarRentalForm
         Me.Close()
     End Sub
 
-    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
-
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click, ClearToolStripMenuItem.Click
+        NameTextBox.Clear()
+        StreetTextBox.Clear()
+        CityTextBox.Clear()
+        StateTextBox.Clear()
+        ZipTextBox.Clear()
+        StartingOdometerTextBox.Clear()
+        EndingOdometerTextBox.Clear()
+        DaysRentedTextBox.Clear()
+        DistanceDrivenTextBox.Clear()
+        MilageChargeTextBox.Clear()
+        DayChargeTextBox.Clear()
+        AmountDiscountedTextBox.Clear()
+        TotalChargeTextBox.Clear()
+        AAACheckBox.Checked = False
+        SeniorCheckBox.Checked = False
+        KilometersRadioButton.Checked = False
+        MilesRadioButton.Checked = True
+        NameTextBox.Focus()
     End Sub
 End Class
